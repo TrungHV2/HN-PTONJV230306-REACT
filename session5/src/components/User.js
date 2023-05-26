4045a44de0f2e94d271e7572b60710873bab17e4
Hoàng Văn Trung <trunghv@bachkhoa-aptech.edu.vn>
2023-05-26 21:46:01 +0700

    Session5(1)

--- session5/src/components/User.js
+++ session5/src/components/User.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react'
 
 export default class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    username: 'admin',
+                    email: 'admin@gmail.com',
+                    phone: '0987654321',
+                    password: '1234'
+                },
+                {
+                    id: 2,
+                    username: 'customer',
+                    email: 'customer@gmail.com',
+                    phone: '0986568799',
+                    password: '1234'
+                }
+            ],
+            values: {}
+        }
+    }
+    handleChange = (evt) => {
+        this.setState(prevState => {
+            let obj = {...prevState.values};
+            obj[evt.target.name] = evt.target.value;
+            return { values: obj }
+        })
+        // this.setState({
+        //     values: {...this.state.values, [evt.target.name]: evt.target.value}
+        // });
+        // console.log(this.state.values);
+    }
+
+    handleNew = () => {
+        console.log(this.state.values);
+    }
+
     render() {
         return (
             <div>
                 <form action="">
                     <fieldset>
                         <legend>Thông tin tài khoản</legend>
-                        <p><b>Tên tài khoản</b> <input type="text"/></p>
-                        <p><b>Email</b> <input type="text"/></p>
-                        <p><b>Số điện thoại</b> <input type="text"/></p>
-                        <p><b>Mật khẩu</b> <input type="text"/></p>
-                        <p><b>Xác nhận mật khẩu</b> <input type="text"/></p>
+                        <p><b>Tên tài khoản</b> <input name="username" onKeyUp={this.handleChange} type="text"/></p>
+                        <p><b>Email</b> <input name="email" onKeyUp={this.handleChange} type="text"/></p>
+                        <p><b>Số điện thoại</b> <input name="phone" onKeyUp={this.handleChange} type="text"/></p>
+                        <p><b>Mật khẩu</b> <input name="password" onKeyUp={this.handleChange} type="text"/></p>
+                        <p><b>Xác nhận mật khẩu</b> <input name="confirm" onKeyUp={this.handleChange} type="text"/></p>
                         <p>
-                            <button class="btn btn-success">Tạo mới</button>
-                            <button class="btn btn-primary">Lưu</button>
+                            <button type='button' className="btn btn-success" onClick={this.handleNew}>Tạo mới</button>
+                            <button type='button' className="btn btn-primary">Lưu</button>
                         </p>
                     </fieldset>
                 </form>
 
-                <table class="table">
+                <table className="table">
                     <thead>
                         <tr>
                             <th>Tên tài khoản</th>
@@ -29,16 +67,20 @@ export default class User extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>admin</td>
-                            <td>admin@gmail.com</td>
-                            <td>0987654321</td>
-                            <td>
-                                <button class="btn btn-warning">Sửa</button>
-                                {/* <button class="btn btn-primary">Đổi mật khẩu</button> */}
-                                <button class="btn btn-danger">Xóa</button>
-                            </td>
-                        </tr>
+                        {
+                            this.state.users.map(u => 
+                                <tr key={u.id}>
+                                    <td>{u.username}</td>
+                                    <td>{u.email}</td>
+                                    <td>{u.phone}</td>
+                                    <td>
+                                        <button className="btn btn-warning">Sửa</button>
+                                        {/* <button class="btn btn-primary">Đổi mật khẩu</button> */}
+                                        <button className="btn btn-danger">Xóa</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>
